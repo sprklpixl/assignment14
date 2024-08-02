20fcce9e885f2c5833cdbc09c1f4cdb75b0a467d
sprklpixl <84677389+sprklpixl@users.noreply.github.com>
2024-08-02 16:05:44 -0500

    changed BrowserRouter to HashRouter in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import Skills from './pages/Skills';
 import Resources from './pages/Resources';
